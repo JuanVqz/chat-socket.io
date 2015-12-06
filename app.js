b992b27609743fd1472d01da737d2ebe7675cd62
juanvqz <juaraldo@hotmail.com>
2015-12-05 23:32:01 -0600

    cambie de nombre el archivo js

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ server.listen(3000);
 console.log('servidor corriendo...');
 
 io.on('connection', function (socket){
-	console.log('servidor detecta nuevo socket');
+	console.log('socket');
 
 	socket.on('join', function (data) {
-		console.log(data);
+		console.log($('#new_user').val());
 	});
+	
 	socket.on('messages', function (message) {
 		socket.emit('broad', message);
 		socket.broadcast.emit('broad', message);
